82f29f3e01e3434ce7d17714d78d80449d6a111e
Crowdbotics <team@crowdbotics.com>
2020-09-17 22:58:00 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/burnt_20396/MainActivity.java
@@ -0,0 +1,15 @@
+package com.burnt_20396;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "burnt_20396";
+    }
+}
